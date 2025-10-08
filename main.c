5c6244945bc12471ff06ead13ef39beab4922231
root <thomaspitet.pro@gmail.com>
2025-10-08 12:19:35 +0000

    etape 3 : recuprer les infos

--- main.c
+++ main.c
@@ -2,8 +2,34 @@
 #include <stdio.h>
 
 int main() {
-	int number;
+	int number1;
+	int number2
 	char symbol;
 
-return(0)
+	printf("Bonjour\n");
+
+	printf("Choisisser une opération : +, -, *, /,\n");
+	scanf("%c", &symbol);
+
+	if (symbol == '+') {
+                printf("Vous avez choisi d'effectuer une addition\n");
+        }
+        else if(symbol == '*') {
+                printf("Vous avez choisi d'effectuer une multiplication\n");
+        }
+        else if(symbol == '-') {
+                printf("Vous avez choisi d'effectuer une soustraction\n");
+        }
+        else (symbol == 42) {
+                printf("Vous avez choisi d'effectuer une multiplication\n");
+        }
+
+	printf("Choisisser un premier nombre\n"),
+	scanf("%c", &number1);
+
+	printf("Choisisser un second nombre\n");
+	scanf("%c", &number2);
+
+return(0);
+
 }
